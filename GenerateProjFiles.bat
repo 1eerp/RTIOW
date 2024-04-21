62e819954eb2a6fb16f0dd31f7749e2cceb64520
Preet <w3irdg@gmail.com>
2024-04-21 17:16:52 -0400

    FIX: GenerateProjFiles.bat now creates the output directory for rendered images

--- GenerateProjFiles.bat
+++ GenerateProjFiles.bat
@@ -1,5 +1,5 @@
 @echo off
-
 call premake5.exe vs2022
-
+cd RTIOW/Source/
+md output
 PAUSE
--- RTIOW/Source/Ray.h
+++ RTIOW/Source/Ray.h
@@ -4,14 +4,14 @@
 class Ray {
 public:
     Ray() {}
-    Ray(const glm::vec3& origin, const glm::vec3& direction) : orig(origin), dir(direction) {}
+    Ray(const glm::vec3& origin, const glm::vec3& direction) : m_origin(origin), m_direction(direction) {}
 
-    const glm::vec3& Origin() const { return orig; }
-    const glm::vec3& Direction() const { return dir; }
+    const glm::vec3& Origin() const { return m_origin; }
+    const glm::vec3& Direction() const { return m_direction; }
 
-    glm::vec3 At(float t) const { return orig + t * dir;}
+    glm::vec3 At(float t) const { return m_origin + t * m_direction;}
 
 private:
-    glm::vec3 orig;
-    glm::vec3 dir;
+    glm::vec3 m_origin;
+    glm::vec3 m_direction;
 };
