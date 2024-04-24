dfadebe66a408a030dacac2ce17e8dd93af9fb82
Preet <w3irdg@gmail.com>
2024-04-23 20:03:57 -0400

    Minor refactor of ray class

--- RTIOW/Source/Ray.h
+++ RTIOW/Source/Ray.h
@@ -3,11 +3,13 @@
 
 class Ray {
 public:
-    Ray() {}
     Ray(const glm::vec3& origin, const glm::vec3& direction) : m_origin(origin), m_direction(direction) {}
 
-    const glm::vec3& Origin() const { return m_origin; }
-    const glm::vec3& Direction() const { return m_direction; }
+    inline const glm::vec3& Origin() const { return m_origin; }
+    inline const glm::vec3& Direction() const { return m_direction; }
+    
+    inline void SetOrigin(glm::vec3 origin) { m_origin = origin; }
+    inline void SetDirection(glm::vec3 direction) { m_direction = direction;}
 
     glm::vec3 At(float t) const { return m_origin + t * m_direction;}
 
